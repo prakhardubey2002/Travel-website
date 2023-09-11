e6093701a20375be7c65068acda466bb3024e310
Prakhar Dubey <prakhardubey2002@gmail.com>
2023-09-11 23:30:47 +0530

    Sticky Banner added,orientation changed,footer removed,client changes added

--- js/app.js
+++ js/app.js
@@ -58,3 +58,45 @@ var swiper = new Swiper(".slide-container", {
         // Initially show the "Description" content
         filterData('option1');
     });
+    var countDownDate = new Date("Oct 30, 2023 00:00:00").getTime();
+    // Update the count down every 1 second
+    var current =new Date().getTime();
+    
+    if(current == countDownDate || current > countDownDate){
+      document.getElementById("counterbox").style.display="none";
+      console.log(`Date : ${countDownDate} and current : ${current}`)
+    }else{
+      console.log("None")
+      console.log(`Date : ${countDownDate} and current : ${current}`)
+    }
+    var x = setInterval(function() {
+    
+      // Get the current time
+      var now = new Date().getTime();
+    
+      // Find the distance between current time and the count down date
+      var distance = countDownDate - now;
+    
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+      // Display the result in the corresponding elements
+      document.getElementById("days").innerHTML = days < 10 ? '0' + `${days}  : ` : `${days}  : `;
+      document.getElementById("hours").innerHTML = hours < 10 ? '0' + `${hours} : ` : `${hours} : `;
+      document.getElementById("minutes").innerHTML = minutes < 10 ? '0' + `${minutes} : ` : `${minutes} : `;
+      document.getElementById("seconds").innerHTML = seconds < 10 ? '0' + seconds : seconds;
+      
+    
+      // If the count down is finished display Happy New Year text
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("title").innerHTML = " <p>Discount Ended</p>";
+        document.getElementById("days").innerHTML = "00";
+        document.getElementById("hours").innerHTML = "00";
+        document.getElementById("minutes").innerHTML = "00";
+        document.getElementById("seconds").innerHTML = "00";
+      }
+    }, 1000);
